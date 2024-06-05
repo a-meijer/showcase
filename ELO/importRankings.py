09bdce83bc2919c36ec2003805bde593f6e5f970
Andrew Meijer <andrew.tr.meijer@gmail.com>
2024-06-05 15:54:01 -0700

    completed basic CSV input and output

--- ELO/importRankings.py
+++ ELO/importRankings.py
@@ -1,14 +1,23 @@
 import csv
 
+input_filename = 'inputMatches.csv'
+output_filename = 'inputRankings.csv'
+
+unique_names = set()
+
 # Open the CSV file
-with open('inputRankings.csv', newline='') as csvfile:
+with open(input_filename, newline='') as csvfile:
     # Create a CSV reader object
     csv_reader = csv.reader(csvfile)
     
     # Iterate over each row in the CSV file
     for row in csv_reader:
         # Do something with the row data
-        print(row)
-        # Print each element of the row
         for element in row:
-            print(element)
+            unique_names.add(element.strip())
+
+# Open/Create the output file  
+with open(output_filename, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    for element in unique_names:
+        writer.writerow([element, 1000])
